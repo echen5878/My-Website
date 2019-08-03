afa21d83a2956ef014c878e7210123e7f009421f
Eric Chen <ebc48@cornell.edu>
2019-08-02 19:46:38 -0500

    Updates

---
+++ precache-manifest.c2b003d4d7e5dde41758a1c6726d1356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70dc4330c5884f048f0e52f3075d7ea1",
+    "url": "/My-Website/index.html"
+  },
+  {
+    "revision": "1ef9bce19ac36635921c",
+    "url": "/My-Website/static/css/2.22921dbe.chunk.css"
+  },
+  {
+    "revision": "1ecc16395a8184294104",
+    "url": "/My-Website/static/css/main.4ac1b3aa.chunk.css"
+  },
+  {
+    "revision": "1ef9bce19ac36635921c",
+    "url": "/My-Website/static/js/2.f144b479.chunk.js"
+  },
+  {
+    "revision": "1ecc16395a8184294104",
+    "url": "/My-Website/static/js/main.fe6315be.chunk.js"
+  },
+  {
+    "revision": "baa753af45f940ad2d57",
+    "url": "/My-Website/static/js/runtime~main.ae1908d6.js"
+  }
+]);
